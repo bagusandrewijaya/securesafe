3c9b2218abf3dc633fbea9476080b682570ab9d4
Bagus Andre Wijaya <103176578+bagusandrewijaya@users.noreply.github.com>
2024-12-24 13:33:05 +0700

    fix gradle

--- safesecurelibs/build.gradle.kts
+++ safesecurelibs/build.gradle.kts
@@ -24,18 +24,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
-publishing {
-    publications{
-        register<MavenPublication>("release") {
-            afterEvaluate { from(components["release"]) }
-        }
-    }
-}
+
 dependencies {
 
     implementation("com.android.support:appcompat-v7:28.0.0")
