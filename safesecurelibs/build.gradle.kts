2f3988909b21ec68249697edb694c6c4768efadd
Bagus Andre Wijaya <103176578+bagusandrewijaya@users.noreply.github.com>
2024-12-24 11:57:19 +0700

    fix jitpack

--- safesecurelibs/build.gradle.kts
+++ safesecurelibs/build.gradle.kts
@@ -29,6 +29,13 @@ android {
     }
 }
 
+publishing {
+    publications{
+        register<MavenPublication>("release") {
+            afterEvaluate { from(components["release"]) }
+        }
+    }
+}
 dependencies {
 
     implementation("com.android.support:appcompat-v7:28.0.0")
